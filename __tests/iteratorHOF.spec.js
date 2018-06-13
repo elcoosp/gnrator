46c1405cf7af9dcbb0a753e45c736980d8bebd53
Elcoosp <elcoosp@gmail.com>
2018-06-13 22:47:15 +0200

    test(Fix test for new implementation):

--- __tests/iteratorHOF.spec.js
+++ __tests/iteratorHOF.spec.js
@@ -9,7 +9,7 @@ function* dummyGen(n) {
 const double = x => x * 2
 const minus = n => x => x - n
 test('should wrap an iterable in a HOF', () => {
-  const it = iteratorHOF(double)(dummyGen, 4)
+  const it = iteratorHOF(double)(dummyGen)[Symbol.iterator](4)
 
   expect(it.next().value).toBe(0)
   expect(it.next().value).toBe(2)
@@ -17,9 +17,14 @@ test('should wrap an iterable in a HOF', () => {
   expect(it.next().value).toBe(6)
   expect(it.next().done).toBe(true)
 })
+test('should wrap an iterable in a HOF and return aother iterable', () => {
+  const it = iteratorHOF(double)(dummyGen)[Symbol.iterator]
+
+  expect([...it(4)]).toEqual([0, 2, 4, 6])
+})
 
 test('should wrap an iterable in successive HOFs', () => {
-  const it = iteratorHOFsPipe(double, minus(4))(dummyGen, 4)
+  const it = iteratorHOFsPipe(double, minus(4))(dummyGen)[Symbol.iterator](4)
 
   expect(it.next().value).toBe(-4)
   expect(it.next().value).toBe(-2)
